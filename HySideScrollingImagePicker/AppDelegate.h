7b87bb8f7e5e6e51aea8f23c3031f2774b659717
Apple <apple@AppledeMacBook-Air.local>
2015-06-30 11:24:36 +0800

    Initial Commit

---
+++ HySideScrollingImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HySideScrollingImagePicker
+//
+//  Created by Apple on 15/6/30.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
